079904c79eabd2c5e1bfaa38639681856facf702
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2025-03-07 12:08:08 +0530

    add port hardly

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Add this line at the top
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT; // Fallback to 4000 if PORT is not set
+const port = process.env.PORT || 4000; // Fallback to 4000 if PORT is not set
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
