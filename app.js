c592dbb4279d4c8e958a458d3753ef0a27339c30
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2025-03-07 12:23:40 +0530

    just a push

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000; // Fallback to 4000 if PORT is not set
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World 2.0!");
 });
 
 app.listen(port, () => {
